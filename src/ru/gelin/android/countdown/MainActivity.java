00ae14d57dd96509fd43a16fd7d60bea280cf38e
Denis Nelubin <dnelubin@gmail.com>
2014-05-27 11:06:15 +0700

    Implemented start/stop by long click.

--- src/ru/gelin/android/countdown/MainActivity.java
+++ src/ru/gelin/android/countdown/MainActivity.java
@@ -8,11 +8,12 @@ import android.view.View;
 import antistatic.spinnerwheel.AbstractWheel;
 import antistatic.spinnerwheel.adapters.NumericWheelAdapter;
 
-public class MainActivity extends Activity implements View.OnSystemUiVisibilityChangeListener {
+public class MainActivity extends Activity implements View.OnSystemUiVisibilityChangeListener, View.OnLongClickListener {
 
     static final int MAX_OFFSET = 99 * 60 + 59;
 
     Timer timer;
+    UpdateTask updater;
 
     /**
      * Called when the activity is first created.
@@ -31,12 +32,11 @@ public class MainActivity extends Activity implements View.OnSystemUiVisibilityC
         View content = findViewById(android.R.id.content);
         content.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
         content.setOnSystemUiVisibilityChangeListener(this);
+        content.setOnLongClickListener(this);
 
         //TODO remove test
         this.timer.set(-30);
         this.timer.reset();
-        this.timer.start();
-        new UpdateTask().execute();
     }
 
     void initWheel(int id, int min, int max) {
@@ -46,12 +46,54 @@ public class MainActivity extends Activity implements View.OnSystemUiVisibilityC
         adapter.setItemTextResource(R.id.text);
         wheel.setViewAdapter(adapter);
         wheel.setCyclic(true);
+        wheel.setLongClickable(true);
+        wheel.setOnLongClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateWheels();
     }
 
     @Override
     public void onSystemUiVisibilityChange(int i) {
         View content = findViewById(android.R.id.content);
         content.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        if (this.timer.isRunning()) {
+            stop();
+        } else {
+            start();
+        }
+        return true;
+    }
+
+    void start() {
+        enableWheel(R.id.ten_mins, false);
+        enableWheel(R.id.mins, false);
+        enableWheel(R.id.ten_secs, false);
+        enableWheel(R.id.secs, false);
+        this.timer.start();
+        this.updater = new UpdateTask();
+        this.updater.execute();
+    }
+
+    void stop() {
+        this.timer.stop();
+        this.updater.stop();
+        enableWheel(R.id.ten_mins, true);
+        enableWheel(R.id.mins, true);
+        enableWheel(R.id.ten_secs, true);
+        enableWheel(R.id.secs, true);
+    }
+
+    void enableWheel(int id, boolean enabled) {
+        AbstractWheel wheel = (AbstractWheel)findViewById(id);
+        wheel.setEnabled(enabled);
     }
 
     void updateWheels() {
@@ -65,7 +107,7 @@ public class MainActivity extends Activity implements View.OnSystemUiVisibilityC
         int mins = offset / 60;
         int secs = offset % 60;
 
-//        Log.d(Tag.TAG, String.format("%d mins, %d secs", mins, secs));
+        Log.d(Tag.TAG, String.format("%d mins, %d secs", mins, secs));
         updateWheel(R.id.ten_mins, mins / 10);
         updateWheel(R.id.mins, mins % 10);
         updateWheel(R.id.ten_secs, secs / 10);
@@ -79,16 +121,23 @@ public class MainActivity extends Activity implements View.OnSystemUiVisibilityC
 
     class UpdateTask extends AsyncTask<Void, Void, Void> {
 
+        boolean run = true;
+
+        public void stop() {
+            this.run = false;
+        }
+
         @Override
         protected Void doInBackground(Void... voids) {
-            while(true) {
+            while(this.run) {
                 try {
-                    Thread.sleep(1000);
                     publishProgress();
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     //nothing to do
                 }
             }
+            return null;
         }
 
         @Override
