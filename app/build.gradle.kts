e3ec9e9dc025609c9f55ffed0a93d578b372c4de
Denis Nelubin <dnelubin@gmail.com>
2026-03-22 21:23:35 +1300

    Change build apk name.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,12 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    applicationVariants.all {
+        outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output.outputFileName = "countdown-timer-${versionName}-${name}.apk"
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
